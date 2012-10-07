6dd113c32b44fb515f777499ac2218bc7c8f545a
Miguel Michelson <miguelmichelson@gmail.com>
2012-10-07 19:09:38 -0300

    midi

--- lib/oscflow-client.rb
+++ lib/oscflow-client.rb
@@ -6,5 +6,6 @@ module Oscflow
   module Client
     ROOT_PATH = Pathname(__FILE__).dirname.expand_path
     autoload  :CLI,           'oscflow-client/cli.rb'
+    autoload  :Midi,          'oscflow-client/midi.rb'
   end
 end
