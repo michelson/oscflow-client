718b80494077a761efab73279a1fa5c64755c22e
Miguel Michelson <miguelmichelson@gmail.com>
2012-10-07 19:51:27 -0300

    osc client methods

--- lib/oscflow-client/osc_send.rb
+++ lib/oscflow-client/osc_send.rb
@@ -2,6 +2,14 @@ module Oscflow
   module Client
     
     class OscSend
+      def connect(host="localhost",port=8080)
+        @client = OSC::Client.new( host, port )
+        puts "Connected to #{host}:#{port}"
+      end
+      
+      def send_data(channel, message)
+        @client.send( OSC::Message.new( "/#{channel}" , message ))
+      end
       
     end
   end
